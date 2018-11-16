217aef78496938f874be7bb9d467834e4c302e3f
Astemir Eleev <astemireleev@gmail.com>
2018-11-16 11:17:00 +0300

    Initial Commit

---
+++ device-kit/device_kit.h
@@ -0,0 +1,19 @@
+//
+//  device_kit.h
+//  device-kit
+//
+//  Created by Astemir Eleev on 16/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for device_kit.
+FOUNDATION_EXPORT double device_kitVersionNumber;
+
+//! Project version string for device_kit.
+FOUNDATION_EXPORT const unsigned char device_kitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <device_kit/PublicHeader.h>
+
+
