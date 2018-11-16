17a7ab97e483096c0779db6e1444d95c2ef7783e
Astemir Eleev <astemireleev@gmail.com>
2018-11-16 13:53:54 +0300

    Updated project settings

--- device-kitTests/device_kitTests.swift
+++ device-kitTests/device_kitTests.swift
@@ -19,11 +19,6 @@ class device_kitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
